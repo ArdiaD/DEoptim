58bdc64c6be56ad5dd65fd68c43e743fadd32fdb
Katharine Mullen <mullenkate@gmail.com>
2010-10-14 15:00:23 +0000

    remove uninitialized P variable in evaluate
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/deoptim/pkg/DEoptim@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/evaluate.c
+++ src/evaluate.c
@@ -5,23 +5,22 @@
 
 double evaluate(long *l_nfeval, double *param, SEXP par, SEXP fcall, SEXP env)
 {
-   int i, P;
+   int i;
    SEXP sexp_fvec, fn;
    double f_result;  
 
    for (i = 0; i < nrows(par); i++) {
       NUMERIC_POINTER(par)[i] = param[i];
    }
-   PROTECT(fn = lang2(fcall, par)); P++;
+   PROTECT(fn = lang2(fcall, par)); 
       (*l_nfeval)++;  /* increment function evaluation count */
 
-   PROTECT(sexp_fvec = eval(fn, env)); P++;
+   PROTECT(sexp_fvec = eval(fn, env)); 
    f_result = NUMERIC_POINTER(sexp_fvec)[0];
-
+   
+   UNPROTECT(2);
    if(ISNAN(f_result))
      error("NaN value of objective function! \nPerhaps adjust the bounds.");
-
-   UNPROTECT(P);
-
+   
    return(f_result); 
 }
