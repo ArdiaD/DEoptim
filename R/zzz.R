5dfcbc61c8a13480ebe8967dee8bd36655808023
Katharine Mullen <mullenkate@gmail.com>
2013-04-20 06:13:09 +0000

    zzz.R, DESCRIPTION
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/deoptim/pkg/DEoptim@113 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
 ".onLoad" <- function (lib, pkg)
 {
   library.dynam(pkg, pkg, lib)
+  
+}
+".onAttach" <- function (lib, pkg)
+{
   packageStartupMessage("\nDEoptim package",
    "\nDifferential Evolution algorithm in R",
    "\nAuthors: D. Ardia, K. Mullen, B. Peterson and J. Ulrich\n")
