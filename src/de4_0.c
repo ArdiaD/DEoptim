485d1b7e95248fbaf3ae1b127aa0397ed239a1a9
k-m-m <mullenkate@gmail.com>
2022-11-07 19:03:53 -0800

    valgrind reported uninit values; fix

--- src/de4_0.c
+++ src/de4_0.c
@@ -255,9 +255,8 @@ void devol(double VTR, double d_weight, double d_cross, int i_bs_flag,
   }
   PROTECT(sexp_gta_popC = popEvaluate(l_nfeval, sexp_gta_popP,  fcall, rho, 1));
   ngta_popC = REAL(sexp_gta_popC);
-  t_bestC = ngta_popC[0];
-  for (i = 1; i < i_NP; i++) {
-    if (ngta_popC[i] <= t_bestC) {
+  for (i = 0; i < i_NP; i++) {
+    if (i == 0 || ngta_popC[i] <= t_bestC) {
       t_bestC = ngta_popC[i];
       for (j = 0; j < i_D; j++)
         gt_bestP[j]=ngta_popP[i+i_NP*j];
